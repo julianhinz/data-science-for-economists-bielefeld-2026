b27e643fbdedef9f4752497bd2d8a45e65179407
Julian Hinz <julianhinz@gmail.com>
2026-04-15 17:50:07 +0200

    add plot

--- 01-getting-started/code/01-examples.R
+++ 01-getting-started/code/01-examples.R
@@ -7,8 +7,21 @@
 if (!require("pacman")) install.packages("pacman"); library(pacman)
 p_load(ggplot2)
 
+# change working directory to the project root
+setwd("01-getting-started")
+
 # create a simple data frame
 data <- data.frame(
   x = 1:10,
   y = c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
 )
+
+# basic scatter plot
+ggplot(data, aes(x = x, y = y)) +
+  geom_point() +
+  labs(title = "Scatter Plot of x vs y",
+       x = "x values",
+       y = "y values") +
+  theme_minimal()
+
+ggsave("output/scatter_plot.png", width = 6, height = 4)
